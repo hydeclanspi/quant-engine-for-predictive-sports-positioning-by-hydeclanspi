4bdd75df4f8f07edbcd1eaed507460c97ac38bba
hydeclanspi <xuyantong@outlook.com>
2026-02-14 09:21:44 +0000

    copy: widen bottom bar slogan separators

--- dugou-model-centre/src/components/BottomBar.jsx
+++ dugou-model-centre/src/components/BottomBar.jsx
@@ -4,9 +4,9 @@ export default function BottomBar() {
       <div className="app-bottom-bar__inner">
         <span className="app-bottom-bar__text">
           <span className="app-bottom-bar__text-cn">信息期权配仓</span>
-          <span className="app-bottom-bar__dot"> · </span>
+          <span className="app-bottom-bar__dot">·</span>
           <span className="app-bottom-bar__text-en">Precision in uncertainty</span>
-          <span className="app-bottom-bar__dot"> · </span>
+          <span className="app-bottom-bar__dot">·</span>
           <span className="app-bottom-bar__text-cn">不错过任何一次从反共识洞察中套利的机会</span>
         </span>
       </div>
